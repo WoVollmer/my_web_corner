18bea9943c97c972688dc8d91963e4537be52129
Wolfgang Vollmer <wo.volmer@online.de>
2021-08-03 18:53:05 +0200

    update

--- messaage_hello.php
+++ messaage_hello.php
@@ -1,11 +1,12 @@
 <?php
 	echo "Message file: Hello Wolfgang via PHP \n";  // \n funktioniert nicht => mit <br> oder </br>
 	$Ausgabe_mit_Leerzeile = "Ausgabe mit Leerzeile";  # $... Variablendefinition
+	echo "<br>";
 	echo "Message File: Hello Wolfgang <br>$Ausgabe_mit_Leerzeile !! \n";
 	echo "von Message file: nun Umlaute ü ä ß \n\n geht <br>es \$ \\ so so \" weiter</br>";
-	echo 'Message-hello.php file: Hallo Welt!<br>'; 
-	echo "Message file: Hallo Welt!";
-	echo "<br>";
+	echo 'File: Message-hello.php<br>'; 
+	echo "Das war der ganze Text";
+	// echo "<br>";
 	/* mehrzeiliger
-	Kommentas */
+	Kommentar */
 ?>
